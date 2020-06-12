6cfa2cb42b0cfabe6390c7c4b1c6033e4e02a0a0
Rajesh Manne <mannerajesh99@gmail.com>
2020-06-12 18:37:20 +0530

    first-commit

--- src/App.js
+++ src/App.js
@@ -6,14 +6,26 @@ import CartContainer from "./components/CartContainer";
 import cartItems from "./cart-items";
 // redux stuff
 
+import { INCREASE, DECREASE } from "./action";
+import reducer from "./reducer";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+const initialState = {
+  cart: cartItems,
+  total: 0,
+  amount: 0,
+};
+const store = createStore(reducer, initialState);
+
 function App() {
   // cart setup
 
   return (
-    <main>
+    <Provider store={store}>
       <Navbar />
+
       <CartContainer cart={cartItems} />
-    </main>
+    </Provider>
   );
 }
 
